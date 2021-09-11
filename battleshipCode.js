8965c72d3ad940c14c44c3c1078fb352966ad8a7
ColeStin <stinsonca16@gmail.com>
2021-09-11 16:35:20 -0500

    added different images for the targeting board, upgraded targetting for both players, with corret naming conventions for the ID of the object, also added a list of a few things that needs to be done before we work on the display board for the ships and such

--- battleshipCode.js
+++ battleshipCode.js
@@ -1,40 +1,122 @@
-var target = '';
+var turn=0;
+var target;
+var player1TargetArray;
+var player2TargetArray;
+var xsize = 9;
+var ysize = 10;
 function hoverOverId(id){
     $("#"+id).css("border-color", "red");
     $("#"+id).css("border-style", "solid");
 }
 
 function mouseOutOfBox(id){
-    $("#"+id).css("border-color", "blue");
+    $("#"+id).css("border-color", "lightblue");
 }
 
 function mouseClick(id){
-    if(target!=''){
-        $("#"+target).css("background-color", "blue");
+    var posType = getSpace(id);
+    if(posType == "empty"){
+        console.log("previous target: "+ target);
+        if(target != undefined){
+            console.log("changing previous space");
+            $("#"+target).css("background-image", 'url("wave.jpg")');
+        }
+        console.log("setting target block: "+ id);
+        target = id;
+        $("#"+id).css("background-image", "url('target.png')");
+        
     }
-    console.log("setting target block");
-    $("#"+id).css("background-color", "orange");
-    target = id;
+    
 }
 
 $(document).ready(function(){
-    for(var x =0; x<9;x++){
-        var html = `
-        <div id='row`+x+`' class='row' style='padding:px'>
-        </div>`
-        $("#primary").append(html);
-        for(var y =0; y<10; y++){
-            var letter = (y+10).toString(36);
-            console.log(letter);
-            var innerHTML =`
-            <div id='col`+x+letter+`' class='selectionBox' onmouseover="hoverOverId('col`+x+letter+`')" onmouseout="mouseOutOfBox('col`+x+letter+`')" onclick="mouseClick('col`+x+letter+`')" >
-                `+x+letter+`
-                </div>
-            `;
-            $("#row"+x).append(innerHTML);
-        }
-    }
+
+    generatePlayer1Targeting();
+    generatePlayer2Targeting();
+    //need to fill in the array with a bucnh of "empty" slots so the targeting knows, specifications: hit = red, miss = gray, empty = blue
+    // hit is a specified to show that when fired, there was an enemy ship that was hit, miss is just a fire with a miss.
     console.log("ready")
 });
 
+function generatePlayer1Targeting(){
+    player1TargetArray = new Array(xsize);
+    for(var x =0; x<xsize;x++){
+        var html = `
+        <div id='player1_row`+x+`' class='row' style='padding:px'>
+        </div>`
+        $("#player1TargetArray").append(html);
+        for(var y =0; y<ysize; y++){
+            player1TargetArray[y] = new Array(ysize);
+            var letter = (y+10).toString(36);
+            var innerHTML =`
+            <div id='player1_`+x+letter+`' class='selectionBox' onmouseover="hoverOverId('player1_`+x+letter+`')" onmouseout="mouseOutOfBox('player1_`+x+letter+`')" onclick="mouseClick('player1_`+x+letter+`')">
+            
+            </div>
+            `;
+            $("#player1_row"+x).append(innerHTML);
+        }
+    }
+    for(var x =0; x<xsize;x++){
+        for(var y =0; y<ysize; y++){
+            player1TargetArray[x][y] = "empty";
+        }
+    }
+}
+function generatePlayer2Targeting(){
+    player2TargetArray = new Array(xsize);
+    for(var x =0; x<xsize;x++){
+        var html = `
+        <div id='player2_row`+x+`' class='row' style='padding:px'>
+        </div>`
+        $("#player2TargetArray").append(html);
+        for(var y =0; y<ysize; y++){
+            player2TargetArray[y] = new Array(ysize);
+            var letter = (y+10).toString(36);
+            var innerHTML =`
+            <div id='player2_`+x+letter+`' class='selectionBox' onmouseover="hoverOverId('player2_`+x+letter+`')" onmouseout="mouseOutOfBox('player2_`+x+letter+`')" onclick="mouseClick('player2_`+x+letter+`')">
+            
+            </div>
+            `;
+            $("#player2_row"+x).append(innerHTML);
+        }
+    }
+    for(var x =0; x<xsize;x++){
+        for(var y =0; y<ysize; y++){
+            player2TargetArray[x][y] = "empty";
+        }
+    }
+}
+
+function getXCoordinate(){
+
+}
+function getYCoordinate(){
+
+}
+function getSpace(id){
+    var player = id.substring(6,7);
+    var x = id.substring(8,9);
+    var y = id.charCodeAt(9)-97;
+    if(player == 1){
+        return (player1TargetArray[x][y]);
+    }
+    else{
+        return (player2TargetArray[x][y]);
+    }
+}
+
+//needed funcitons->
+/*
+fire function: takes in and updates array for whatever the target was for the player
+there needs to be a placement function for stuff, probably a modal to select settins and such at begining of the game
+function to check target array for specific player
+
+images needed->
+waves
+target
+all 6 ships
+battleship logo
+gray square for miss
+
+*/
 
