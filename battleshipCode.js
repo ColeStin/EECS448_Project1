a77f3d502a388f5e01c32ab4ea98a8081bd7505c
ColeStin <stinsonca16@gmail.com>
2021-09-19 00:43:28 -0500

    just minor changes to funcitns

--- battleshipCode.js
+++ battleshipCode.js
@@ -67,7 +67,7 @@ function mouseClick(id){
 
 function fireRound(player){
 console.log("fired " + player);
-$("#player1display_0a").css("background-color", "red")
+$("#1Assault-0").css("background-image", "url(target.png)")
 }
 
 $(document).ready(function(){
@@ -232,7 +232,7 @@ function getValidPlacement(shipType, placeSpace){
             shiplength = 5;
             break;
         case "Carrier":
-            
+            shiplength = 6;
             break;
         default:
             shiplength = 1;
@@ -373,11 +373,14 @@ function dragDrop(ev, id) {
             case '1':
                 console.log("player 1")
                 for(i = 0; i<= getShipLength(typeOfShip.substring(1)); i++){
+                    var shipConcant = typeOfShip+"-"+i;
                     if(true){//get orintation and switch stuff
-                        player1DisplayArray[getXCoordinate(id)][getYCoordinate(id)+i] = typeOfShip.substring(1);
+                        //horizontal
+                        player1DisplayArray[getXCoordinate(id)][getYCoordinate(id)+i] = shipConcant;
                     }
                     else{
-                        player1DisplayArray[getXCoordinate(id)+i][getYCoordinate(id)] = typeOfShip.substring(1);
+                        //vertical
+                        player1DisplayArray[getXCoordinate(id)+i][getYCoordinate(id)] = shipConcant;
                     }
                     
                 }
@@ -386,10 +389,10 @@ function dragDrop(ev, id) {
             case '2':
                 for(i = 0; i<= getShipLength(typeOfShip.substring(1)); i++){
                     if(true){//get orintation and switch stuff
-                        player2DisplayArray[getXCoordinate(id)][getYCoordinate(id)+i] = typeOfShip.substring(1);
+                        player2DisplayArray[getXCoordinate(id)][getYCoordinate(id)+i] = shipConcant;
                     }
                     else{
-                        player2DisplayArray[getXCoordinate(id)+i][getYCoordinate(id)] = typeOfShip.substring(1);
+                        player2DisplayArray[getXCoordinate(id)+i][getYCoordinate(id)] = shipConcant;
                     }
                 }
                 console.log('player 2');
